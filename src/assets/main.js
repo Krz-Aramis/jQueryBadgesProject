858d5235b17a953b7ba651cbb57779146d33b6d9
Dr. Lionel Saliou <lionel.saliou@gmail.com>
2018-01-20 17:42:42 +0000

    Complete Project
    Note to self: really learn jQuery. Whilst it was easy knowing
    what to do. It was much harder figuring out HOW TO do it.
    In fact, this commmit might not meet the requirements from the
    control script.
    
    Most importantly perhaps, the Javascript code is not very
    object oriented. The majority of the code, handcraft the HTML
    code which is not a good long terms solution.
    
    Perhaps in the next commit I can try to create a new element
    in the same fashion as 'newDiv' with the properties and all.
    
       --- Lionel Saliou, Ph.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,49 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/4257681.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // grab the correct html element to update
+      var targetDiv = $( "#badges" ) ;
+      var iCount = 0;
+      // handle response
+      if (response.courses.completed != null) {
+        // For each results in the set
+        $.each(response.courses.completed, function() {
+          // this refers to the current record being parsed.
+          ++iCount;
+          newDiv = $('<div/>', {
+            id: iCount,
+            class: 'course'
+          });
+          newDiv.append("<h3>" + this.title +"</h3>")
+                .append("<br />")
+                .append("<img src='" + this.badge + "' />")
+                .append("<br />")
+                .append("<a class='btn btn-primary' href='" + this.url + "' target='_blank'>See Course</a>") ;
+
+          targetDiv.append(newDiv);
+          
+          /*targetDiv.add( "div" ).addClass( "course" )
+                                .add("h3").text(this.title)
+                                .append("<img src='" + this.badge + "' />")
+                                .append("<a href='" + this.url + "' target='_blank'>See Course</a>") ;
+          */
+          /*targetDiv.html("<div class='course'>" + 
+                           "<h3>" + this.title + "</h3>" +
+                           "<img src='" + this.badge + "' />" +
+                           "<a href='" + this.url + "' target='_blank'>See Course</a>" +
+                         "</div>");*/
+        })
+      }
+      else { 
+        console.log("failed to obtained badges!") ; 
+        targetDiv.text("failed to obtained badges!") ;
+      }
+    },
+  })
+  
 
 });
